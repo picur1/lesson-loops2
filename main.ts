fd9a98e124d534c5a8f85277faf8be51960bb138
picur1 <133989406+picur1@users.noreply.github.com>
2023-06-08 16:58:29 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
+let number = 0
+input.onButtonPressed(Button.A, function () {
+    number = 1
+    while (number <= 7) {
+        basic.showNumber(number)
+        basic.pause(1000)
+        number += 1
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    for (let index = 0; index < 5; index++) {
+        music.playMelody("G B A G C5 B A B ", 120)
+    }
 })
